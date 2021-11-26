d6fb8b01fe8180c9d03b7f9ccc122996caa61ddb
Pradhipta <pradhiptats@gmail.com>
2021-11-26 13:46:45 +0700

    Add edit and delete button on teacher page for testing purposes

--- landingpage/resources/views/teachers/show.blade.php
+++ landingpage/resources/views/teachers/show.blade.php
@@ -4,5 +4,29 @@
 IMAGE = {{$teacher->image_path}}
 <h1>{{$teacher->title}}</h1>
 
+@if (isset(Auth::user()->id) && Auth::user()->id == $teacher->user_id)
+    <span class="float-left">
+        <a 
+            href="/teachers/{{ $teacher->id }}/edit"
+            class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+            Edit
+        </a>
+    </span>
 
+    <span class="float-left">
+         <form 
+            action="/teachers/{{ $teacher->id }}"
+            method="POST">
+            @csrf
+            @method('delete')
+
+            <button
+                class="text-red-500 pr-3"
+                type="submit">
+                Delete
+            </button>
+
+        </form>
+    </span>
+@endif
 @endsection
