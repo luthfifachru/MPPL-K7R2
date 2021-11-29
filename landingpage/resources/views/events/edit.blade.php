f99b7aa89fd80faa35d7f8f8e966d297fb9b72b7
AlfianArief <alfian_arief@apps.ipb.ac.id>
2021-11-29 12:08:12 +0700

    edit page

--- landingpage/resources/views/events/edit.blade.php
+++ landingpage/resources/views/events/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="w-4/5 m-auto text-left">
     <div class="py-15">
         <h1 class="text-6xl">
-            Edit Event
+            Edit Events
         </h1>
     </div>
 </div>
@@ -21,7 +21,7 @@
     </div>
 @endif
 
-<div class="w-4/5 m-auto pt-20">
+<div class="w-4/5 m-auto pt-10 px-5">
     <form 
         action="/events/{{$event->id}}"
         method="POST"
@@ -29,33 +29,29 @@
         @csrf
         @method('PUT')
 
-        <input 
-            type="text"
-            name="title"
-            value="{{$event->title}}"
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+       <input class="form-control form-control-lg" type="text" name="title" value="{{$event->title}}" aria-label=".form-control-lg example">
 
-        <textarea 
-            name="content"
-            placeholder="Content..."
-            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{$event->content}}</textarea>
 
-        <div class="bg-grey-lighter pt-15">
-            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
-                <span class="mt-2 text-base leading-normal">
-                    Select a file
-                </span>
-                <input 
-                    type="file"
-                    name="image"
-                    class="hidden">
-            </label>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label" ></label>
+            <textarea class="form-control" name="content" placeholder="Content..." id="exampleFormControlTextarea1" rows="3">{{$event->content}}</textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="formFile" class="form-label"></label>
+            <input class="form-control" type="file" name="image" id="formFile">
         </div>
 
         <button    
             type="submit"
-            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
             Submit Post
+        </button>
+
+        <button    
+            type="delete"
+            class="uppercase mt-15 bg-gray-500 text-blue-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
+            Delete
         </button>
     </form>
 </div>
--- landingpage/resources/views/teachers/edit.blade.php
+++ landingpage/resources/views/teachers/edit.blade.php
@@ -21,7 +21,7 @@
     </div>
 @endif
 
-<div class="w-4/5 m-auto pt-20">
+<div class="w-4/5 m-auto pt-5">
     <form 
         action="/teachers/{{$teacher->id}}"
         method="POST"
@@ -29,33 +29,24 @@
         @csrf
         @method('PUT')
 
-        <input 
-            type="text"
-            name="title"
-            value="{{$teacher->title}}"
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
 
-        <textarea 
-            name="content"
-            placeholder="Content..."
-            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{$teacher->content}}</textarea>
+        <input class="form-control form-control-lg" type="text" name="title"  value="{{$teacher->title}}" aria-label=".form-control-lg example">
 
-        <div class="bg-grey-lighter pt-15">
-            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
-                <span class="mt-2 text-base leading-normal">
-                    Select a file
-                </span>
-                <input 
-                    type="file"
-                    name="image"
-                    class="hidden">
-            </label>
+        <div class="mb-3">
+            <label for="formFile" class="form-label"></label>
+            <input class="form-control" type="file" name="image" id="formFile">
         </div>
 
         <button    
             type="submit"
-            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-bold py-4 px-8 rounded-2xl">
             Submit Post
+        </button>
+
+        <button    
+            type="delete"
+            class="uppercase mt-15 bg-gray-500 text-blue-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
+            Delete
         </button>
     </form>
 </div>
