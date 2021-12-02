0ddbb1f50b762347836b1762edf888b95c8a912a
Pradhipta <pradhiptats@gmail.com>
2021-12-03 05:59:03 +0700

    Return events and data to landing page

--- landingpage/app/Http/Controllers/PagesController.php
+++ landingpage/app/Http/Controllers/PagesController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Teacher;
+use App\Models\Events;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('index')
+        ->with('teachers', Teacher::orderBy('title', 'ASC')->take(6)->get())
+        ->with('events', Events::orderBy('updated_at', 'DESC')->take(6)->get())
+        ;
     }
 }
