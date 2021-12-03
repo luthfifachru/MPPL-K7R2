d157f0fff921659b65fadab43b2c65fde70652ce
Pradhipta <pradhiptats@gmail.com>
2021-12-03 21:45:59 +0700

    add buttons on admin dropdown

--- landingpage/resources/views/layouts/app.blade.php
+++ landingpage/resources/views/layouts/app.blade.php
@@ -78,6 +78,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('user.password.edit') }}"
+                                    >
+                                        {{ __('Change Password') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('register') }}"
+                                    >
+                                        {{ __('Register New Admin') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
