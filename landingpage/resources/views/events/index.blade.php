34db5bf53842264c0bf7c43d69a918a80d80a987
luthfifachru <luthfi_fachru@apps.ipb.ac.id>
2021-12-03 20:44:22 +0700

    fixing app name and UI events page

--- landingpage/resources/views/events/index.blade.php
+++ landingpage/resources/views/events/index.blade.php
@@ -8,8 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
 
   </head>
 <body>
@@ -19,7 +18,9 @@
     <div class="container-fluid py-5 mt-5">
           <div class="container">
             <div class="text-center">
-              <h1 class="mb-5">Event Post</h1>
+              <h2 class="text-4xl font-bold py-10 uppercase">
+            All Events
+        </h2>
             </div>
 
             @if (Auth::check())
